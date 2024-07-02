8374df83a18d9c4f8fc56a1aff7bd226e3dce138
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2024-07-03 02:39:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-
+	str = str.toLowerCase();
+	let rev= "";
+	for(let i=str.length;i>=0;i--){
+		rev += str.charAt(i);
+	}
+	if(rev === str)return true;
+	return false;
 }
 module.exports = palindrome
